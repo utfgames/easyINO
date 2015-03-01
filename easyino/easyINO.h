93cc3e36cd831a1352c8a1f77efbbfb21bafd834
UTFGames <utfgames@gmail.com>
2015-02-28 23:34:46 -0300

    Update easyINO.h

--- easyino/easyINO.h
+++ easyino/easyINO.h
@@ -1,27 +1,29 @@
 /***************UTFGames - UTFPR - Campus Cornélio Procópio*******************
 	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	
 	Developed by Lucas da Cunha Bueno
 *****************************************************************/
-/*---DEFINIÇÕES DE CONSTANTES---*/
+
+/*---DEFINIÇÃO DE CONSTANTES---*/
 #ifndef _easyINO
 #define _easyINO
 #define ENTRADA 0
 #define SAIDA 1
 #define LIGADO 1
 #define DESLIGADO 0
-/*---INCLUSÕES DE BIBLIOTECAS---*/
+
+/*---INCLUSÃO DE BIBLIOTECAS---*/
 #include <Arduino.h>
 #include <String.h>
 
